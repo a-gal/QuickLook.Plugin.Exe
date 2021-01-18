196ea306424bbf4914d3b76a0ad7c0a5bcdbffef
Al <onnotoff@gmail.com>
2021-01-18 17:10:39 +0500

    chore: update file name in pack-zip.ps1

--- Scripts/pack-zip.ps1
+++ Scripts/pack-zip.ps1
@@ -1,5 +1,5 @@
-Remove-Item ..\QuickLook.Plugin.HelloWorld.qlplugin -ErrorAction SilentlyContinue
+Remove-Item ..\QuickLook.Plugin.Exe.qlplugin -ErrorAction SilentlyContinue
 
 $files = Get-ChildItem -Path ..\bin\Release\ -Exclude *.pdb,*.xml
-Compress-Archive $files ..\QuickLook.Plugin.HelloWorld.zip
-Move-Item ..\QuickLook.Plugin.HelloWorld.zip ..\QuickLook.Plugin.HelloWorld.qlplugin
+Compress-Archive $files ..\QuickLook.Plugin.Exe.zip
+Move-Item ..\QuickLook.Plugin.Exe.zip ..\QuickLook.Plugin.Exe.qlplugin
